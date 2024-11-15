2a6e9875c4b962cddbf2cc5d32102114a412c25e
Dylden <dylan.dennig@lapiscine.pro>
2024-11-15 11:45:39 +0100

    localStorage

--- js/index.js
+++ js/index.js
@@ -1,69 +1,95 @@
 //VARIABLES
 
 const houses = [
-    {inputId: 'inputGryffondor', buttonId: 'addGryffondor', scoreId: 'scoreGryffondor'},
-    {inputId: 'inputSerpentard', buttonId: 'addSerpentard',  scoreId: 'scoreSerpentard'},
-    {inputId: 'inputPoufsouffle', buttonId: 'addPoufsouffle',  scoreId: 'scorePoufsouffle'},
-    {inputId: 'inputSerdaigle', buttonId: 'addSerdaigle',  scoreId: 'scoreSerdaigle'}
+    { inputId: 'inputGryffondor', buttonId: 'addGryffondor', scoreId: 'scoreGryffondor' },
+    { inputId: 'inputSerpentard', buttonId: 'addSerpentard', scoreId: 'scoreSerpentard' },
+    { inputId: 'inputPoufsouffle', buttonId: 'addPoufsouffle', scoreId: 'scorePoufsouffle' },
+    { inputId: 'inputSerdaigle', buttonId: 'addSerdaigle', scoreId: 'scoreSerdaigle' }
 ]
 
-let plus = document.querySelector('.logoPlus');
-let scores = document.querySelector('.score');
-
 let scoresInMemory = {
-    Gryffondor: 0,
-    Serpentard:  0,
-    Poufsouffle: 0,
-    Serdaigle: 0
+    Gryffondor: parseInt(localStorage.getItem('Gryffondor')) || 0,
+    Serpentard: parseInt(localStorage.getItem('Serpentard')) || 0,
+    Poufsouffle: parseInt(localStorage.getItem('Poufsouffle')) || 0,
+    Serdaigle: parseInt(localStorage.getItem('Serdaigle')) || 0
 }
+
+updateScores();
 
 //FONCTIONS
 
-function addScore(inputId, buttonId, scoreId){
+function addScore(inputId, scoreId) {
 
+    const houseName = inputId.replace('input', '');
     let inputElement = document.getElementById(inputId);
-
     let scoreElement = document.getElementById(scoreId);
 
     let newScore = parseInt(inputElement.value);
 
-    if(isNaN(newScore) || newScore < 0){
+    if (isNaN(newScore) || newScore < 0) {
         alert("Entre un NOMBRE, Harry !")
         return
     }
 
-    let houseName = inputId.replace('input', '');
-
     scoresInMemory[houseName] += newScore;
     scoreElement.textContent = scoresInMemory[houseName];
     inputElement.value = "";
-    
+
+    localStorage.setItem(houseName, scoresInMemory[houseName]);
+
     ordreClassement(); //Appel de la fonction pour organiser le classement par score.
 }
 
-function ordreClassement(){
+function ordreClassement() {
     let houseList = document.getElementById('houseList');
     let houseItem = Array.from(houseList.children);
 
     houseItem.sort((a, b) => {
         let scoreA = parseInt(a.querySelector('p').textContent)
         let scoreB = parseInt(b.querySelector('p').textContent)
-        return scoreB - scoreA; 
+        return scoreB - scoreA;
     })
 
-    houseItem.forEach(item =>{
-        item.classList.add('moving')
-    })
-    houseItem.forEach(item =>{
+    houseItem.forEach(item => {
         houseList.appendChild(item)
-    
+
+    });
+}
+
+function updateScores() {
+    houses.forEach(house => {
+        const scoreElement = document.getElementById(house.scoreId);
+        houseName = house.inputId.replace('input', '');
+        scoreElement.textContent = scoresInMemory[houseName];
+        ordreClassement();
     });
 }
 
 //APPEL DES FONCTIONS
 
 houses.forEach(house => {
-    document.getElementById(house.buttonId).addEventListener('click',() => {
-        addScore(house.inputId, house.buttonId, house.scoreId.replace('input', ''))
+    const button = document.getElementById(house.buttonId);
+    button.addEventListener('click', () => {
+        addScore(house.inputId, house.scoreId);
     });
 });
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === "Escape") {
+
+        scoresInMemory = {
+            Gryffondor: 0,
+            Serpentard: 0,
+            Poufsouffle: 0,
+            Serdaigle: 0
+        }
+
+        localStorage.setItem('Gryffondor', 0)
+        localStorage.setItem('Serpentard', 0)
+        localStorage.setItem('Poufsouffle', 0)
+        localStorage.setItem('Serdaigle', 0)
+    }
+    ordreClassement();
+
+})
+
